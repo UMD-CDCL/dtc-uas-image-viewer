409757833385b900baa9c2a24a58a4a1d506a797
ctitus1 <ctitus@umd.edu>
2025-06-05 11:13:43 -0400

    it was working, forgot a slash

--- pull_new_images.sh
+++ pull_new_images.sh
@@ -10,13 +10,12 @@ fi
 # Define variables
 REMOTE_USER="j1"
 IP_ADDRESS=$1
-REMOTE_DIR="/home/j1/images"
+REMOTE_DIR="/home/j1/images/"
 LOCAL_DIR="/home/ctitus/DTC/dtc-image-viewer"
 LOG_FILE="$LOCAL_DIR/rsync_log.txt"
 
 # Pull new images from the remote directory and log the names of transferred files
 echo "rsync into $REMOTE_USER@$IP_ADDRESS..."
-#rsync -az --ignore-existing --include='*img[0-9][0-9]*' --exclude=* --progress $REMOTE_USER@$IP_ADDRESS:$REMOTE_DIR $LOCAL_DIR
-rsync -az --ignore-existing --progress $REMOTE_USER@$IP_ADDRESS:$REMOTE_DIR $LOCAL_DIR
+rsync -az --ignore-existing --include='*img[0-9][0-9]*' --exclude=* --progress $REMOTE_USER@$IP_ADDRESS:$REMOTE_DIR $LOCAL_DIR
 
 echo "=== PULL DONE ==="
